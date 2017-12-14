e6f0a28953ae2130380b20868f92a8912d64aab5
Richard5277 <richard.dev@icloud.com>
2017-12-14 16:37:14 -0500

    entry point

---
+++ index.php
@@ -0,0 +1,29 @@
+<? php
+
+require 'vendor/autoload.php';
+
+$session = new SpotifyWebAPI\Session(
+    'client_id',
+    'client_secret',
+    'redirect_uri'
+);
+
+$api = new SpotifyWebAPI\SpotifyWebAPI();
+
+if (isset($_GET['code'])) {
+    $session->requestAccessToken($_GET['code']);
+    $api->setAccessToken($session->getAccessToken());
+
+    print_r($api->me());
+} else {
+    $options = [
+        'scope' => [
+            'user-read-email',
+        ],
+    ];
+
+    header('Location: ' . $session->getAuthorizeUrl($options));
+    die();
+}
+
+?>
